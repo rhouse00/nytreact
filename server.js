ebbc4523602bfc3fd2798bfa9cbd40232a605319
rhouse00 <ryanjameshouse@gmail.com>
2017-02-26 14:13:52 -0800

    missing period for proper heroku port utilization

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const Promise = require('bluebird');
 
 const app = express();
-const PORT = process.envPORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 const Article = require('./models/article');
 
